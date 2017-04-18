e991ae18f06a55ec9a551e1791489bd8690d161b
Hackbright Student <no-reply@hackbrightacademy.com>
2017-04-18 12:53:02 -0700

    Added choice of compliments and insults

--- server.py
+++ server.py
@@ -18,10 +18,21 @@ AWESOMENESS = [
 def start_here():
     """Home page."""
 
-    return "<!doctype html><html>Hi! This is the home page.</html>"
+    return """
+    <!doctype html>
+    <html>
+      <head>
+        <title></title>
+      </head>
+      <body>
+      Hi! This is the home page.
+      <a href="/hello">Hello</a>
+      </body>
+    </html>
+    """
 
 
-@app.route('/hello')
+@app.route('/hello', methods=["GET"])
 def say_hello():
     """Say hello and prompt for user's name."""
 
@@ -35,20 +46,42 @@ def say_hello():
         <h1>Hi There!</h1>
         <form action="/greet">
           What's your name? <input type="text" name="person">
-          <input type="submit" value="Submit">
+          <input type="submit" value="Submit"><br>
+          Select your compliment:<br>
+          <input type="radio" name="awesomeness" value="awesome">AWESOME</input><br>
+          <input type="radio" name="awesomeness" value="terrific">TERRIFIC</input><br>
+          <input type="radio" name="awesomeness" value="fantastic">FANTASTIC</input><br>
+          <input type="radio" name="awesomeness" value="neato">NEATO</input><br>
+          <input type="radio" name="awesomeness" value="oh-so-not-meh">OH-SO-NOT-MEH</input><br>
+          <input type="radio" name="awesomeness" value="ducky">DUCKY</input><br>
+          <input type="radio" name="awesomeness" value="coolio">COOLIO</input><br>
+        </form>
+        <br>Or choose:<br>
+        <form action="diss_person">
+          What's your name <input type="text" name="person">
+          <input type="submit" value="Submit"><br>
+          Select your insult:<br>
+          <input type="radio" name="insults" value="bald">BALD</input><br>
+          <input type="radio" name="insults" value="smelly">SMELLY</input><br>
+          <input type="radio" name="insults" value="illiterate">ILLITERATE</input><br>
+          <input type="radio" name="insults" value="pig">PIG</input><br>
+          <input type="radio" name="insults" value="oh-so-meh">OH-SO-MEH</input><br>
+          <input type="radio" name="insults" value="lame">LAME</input><br>
+          <input type="radio" name="insults" value="boring">BORING</input>
         </form>
       </body>
     </html>
     """
 
 
-@app.route('/greet')
+@app.route('/greet', methods=["GET"])
 def greet_person():
     """Get user by name."""
 
     player = request.args.get("person")
 
-    compliment = choice(AWESOMENESS)
+    # compliment = awesomeness
+    awesomeness = request.args.get('awesomeness')
 
     return """
     <!doctype html>
@@ -57,10 +90,33 @@ def greet_person():
         <title>A Compliment</title>
       </head>
       <body>
-        Hi, {player}! I think you're {compliment}!
+        Hi, {player}! I think you're {awesomeness}!
       </body>
     </html>
-    """.format(player=player, compliment=compliment)
+    """.format(player=player, awesomeness=awesomeness)
+      # , compliment=compliment)
+
+
+@app.route('/diss_person', methods=["GET"])
+def diss_person():
+    """Get user by name."""
+
+    player = request.args.get("person")
+
+    insults = request.args.get('insults')
+
+    return """
+    <!doctype html>
+    <html>
+      <head>
+        <title>A Compliment</title>
+      </head>
+      <body>
+        Hi, {player}! I think you're {insults}!
+      </body>
+    </html>
+    """.format(player=player, insults=insults)
+      # , compliment=compliment)
 
 
 if __name__ == '__main__':
